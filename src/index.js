4ff456647fb9b86a683c7b05448598f8850139d0
romer8 <gio.busrom@gmail.com>
2024-10-28 11:03:55 -0600

    basic configuration for ol with a backend driven approach

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+export {LegendControl} from './components/controls/Legend/Legend';
+export {default as Controls} from './components/controls/Controls';
+export {LayersControl} from './components/controls/Layers/LayersControl';
+
+export {Layers} from './components/layers/Layers';
+export {default as Layer} from './components/layers/Layer';
+
+export {default as Overlay} from './components/overlays/Overlay';
+export {default as Overlays} from './components/overlays/Overlays';
+
+export {default as View} from './components/View';
+
+export {DefaultLayerConfig, DefaultViewConfig,DefaultMapConfig, DefaultLegend, DefaultOverlays} from './data/defaults';
+
+export {MapProvider} from './providers/MapProvider';
+export {useMapContext} from './contexts/MapContext';
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',  
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  externals: {
+    'react': 'commonjs react' 
+  }
+};
